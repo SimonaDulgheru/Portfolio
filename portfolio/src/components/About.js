3d5be4bc5442c1bc6c6c1971f6110d471ca066c2
Simona <s.c.dulgheru@gmail.com>
2020-05-12 06:13:21 +0100

    About page made responsive

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,30 +1,41 @@
 import React from "react";
 import email from "../images/email.png";
 import github from "../images/github.png";
-import linkedIn from "../images/linkedIn.png";
+import linkedIn from "../images/linkedIn2.png";
 import "../css/projects.css";
 
 const About = () => {
 	return (
 		<div>
-			<div className='wrapper-index  wrapper-desktop-index'>
+			<div className="wrapper-index  wrapper-desktop-index">
 				<h2>About Me</h2>
-				<div className='column-text  column-text-desktop-1'>
-					<p id='about-paragraph about-paragraph-desktop '>
+				<div className="column-text  column-text-desktop-1">
+					<p id="about-paragraph about-paragraph-desktop ">
 						Full Stack Developer adept at building responsive Web
 						Applications from Front to Back with medical background.
+						<br />
+						<br />
 						Passionate about learning new technologies, bringing
 						ideas to life, and working with dedicated teams to build
 						efficient and robust applications suited to the user’s
-						needs. I have superior focus and attention to detail,
-						also knowledge of HTML, CSS, JavaScript, ES6+, Node.js,
-						Express.js, React.js, MySQL, MogoDB, Mongoose. In my
-						previous roles, I have been involved in running and
-						managing several projects and teams. I am capable of
+						needs.
+						<br />
+						<br /> I have superior focus and attention to detail,
+						also knowledge of <strong>HTML</strong>,
+						<strong>CSS</strong>, <strong>JavaScript/jQuery</strong>
+						,<strong>ES6+</strong>,{" "}
+						<strong>Node.js/Express.js</strong>,
+						<strong> React.js</strong>,<strong> MySQL</strong>,
+						<strong>MogoDB/ Mongoose</strong> . <br />
+						<br />
+						In my previous roles, I have been involved in running
+						and managing several projects and teams. I am capable of
 						gathering requirements and prioritising tasks in order
-						to deliver value iteratively. My core strengths include,
-						but are not limited to the following:
-						<ul className='skills-list'>
+						to deliver value iteratively. <br />
+						<br />
+						My core strengths include, but are not limited to the
+						following:
+						<ul className="skills-list">
 							<li>
 								Having an approachable and professional manner
 								at all times.
@@ -40,52 +51,50 @@ const About = () => {
 								required.
 							</li>
 						</ul>
-						<p>
-							For the future, I wish to continue learning and grow
-							in a creative and collaborative environment.
-						</p>
+						For the future, I wish to continue learning and grow in
+						a creative and collaborative environment.
 					</p>{" "}
 				</div>
-				<div class='contacts contacts-mobile'>
-					<div class='resume-contact'>
-						<h3 class='resume resume-tablet resume-desktop'>
+				<div className="contacts contacts-mobile">
+					{/* <div className="resume-contact">
+						<h3 className="resume resume-tablet resume-desktop">
 							<a
-								href='https://drive.google.com/file/d/1_q-1aaqGq0epz6FJ-ArKbhkPVG0NWKXE/view?usp=sharing'
-								target='_blank'
-								rel='noopener noreferrer'>
+								href="https://drive.google.com/file/d/1_q-1aaqGq0epz6FJ-ArKbhkPVG0NWKXE/view?usp=sharing"
+								target="_blank"
+								rel="noopener noreferrer">
 								Resume
 							</a>
 						</h3>
-					</div>
-					<div class='contact-links contact-links-mobile'>
+					</div> */}
+					<div className="contact-links contact-links-mobile">
 						<a
-							href='https://github.com/SimonaDulgheru'
-							target='_blank'
-							rel='noopener noreferrer'>
+							href="https://github.com/SimonaDulgheru?tab=repositories"
+							target="_blank"
+							rel="noopener noreferrer">
 							<img
 								src={github}
-								class='icon-github'
-								alt='Github-link'
+								className="icon-github"
+								alt="Github-link"
 							/>
 						</a>
 						<a
-							href='mailto:s.c.dulgheru@gmail.com'
-							target='_blank'
-							rel='noopener noreferrer'>
+							href="mailto:s.c.dulgheru@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer">
 							<img
 								src={email}
-								class=' icon-mail'
-								alt='Email-link'
+								className="icon-mail"
+								alt="Email-link"
 							/>
 						</a>
 						<a
-							href='https://www.linkedin.com/in/simona-dulgheru-36777ba3/'
-							target='_blank'
-							rel='noopener noreferrer'>
+							href="https://www.linkedin.com/in/simona-dulgheru-36777ba3/"
+							target="_blank"
+							rel="noopener noreferrer">
 							<img
 								src={linkedIn}
-								class='icon-linkedin'
-								alt='LinkedIn-link'
+								className="icon-linkedin"
+								alt="LinkedIn-link"
 							/>
 						</a>
 					</div>
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,43 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
 import "../css/style.css";
 
-const Navbar = () => {
-	return (
-		<div>
-			<header className='header header-tablet header-desktop'>
-				<h1 className='logo logo-tablet logo-desktop'>
-					{" "}
-					Simona Dulgheru
-				</h1>
-				<nav className='nav nav-tablet nav-desktop'>
-					<div className='hamburger-menu'>
-						<div className='line line-1'></div>
-						<div className='line line-2'></div>
-						<div className='line line-3'></div>
-					</div>
-					<ul className='nav-links nav-links-tablet nav-links-desktop'>
-						<li className='nav-item'>
-							<Link to='/' className='link'>
-								About
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/portfolio' className='link'>
-								Portfolio
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/contact' className='link'>
-								Contact
-							</Link>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</div>
-	);
-};
+export default class Navbar extends Component {
+	state = { showmenu: false };
 
-export default Navbar;
+	toggleMenu = () => {
+		this.setState({
+			showMenu: !this.state.showMenu,
+		});
+	};
+	render() {
+		const menu = this.state.showMenu ? "hide" : "change";
+		return (
+			<div>
+				<header className="header header-tablet header-desktop">
+					<h1 className="logo logo-tablet logo-desktop">
+						{" "}
+						Simona Dulgheru
+					</h1>
+					<nav className={`nav nav-tablet nav-desktop  ${menu}`}>
+						<div
+							onClick={this.toggleMenu}
+							className={`hamburger-menu `}>
+							<div className="line line-1"></div>
+							<div className="line line-2"></div>
+							<div className="line line-3"></div>
+						</div>
+						<ul
+							className={`nav-links  change nav-links-tablet nav-links-desktop`}>
+							<li className="nav-item">
+								<Link to="/" className="link ">
+									About
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link to="/portfolio" className="link ">
+									Portfolio
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link to="/contact" className="link">
+									Contact
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+		);
+	}
+}
+// const Navbar = () => {
+// 	return (
+
+// 	);
+// };
+
+// export default Navbar;
