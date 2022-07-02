2ee43f8a26dcb844b382951abbb556f4d09455db
Simona <s.c.dulgheru@gmail.com>
2022-07-02 17:50:33 +0300

    Tidying up and update app

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -68,7 +68,7 @@ const About = () => {
 						data-aos="fade-right"
 						className="resume resume-tablet resume-desktop">
 						<a
-							href="https://drive.google.com/file/d/1EzaO6GFFcAXjYcUpp6yIXHejjbnJPeyr/view?usp=sharing"
+							href="https://drive.google.com/file/d/1NmXMqFhJXu0qk9DnsKuKuqyXIuj7cBlL/view?usp=sharing"
 							target="_blank"
 							rel="noopener noreferrer">
 							Resume
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import Navbar from "./Navbar";
-// import "../images/Poll App.jpg";
 
 const Footer = () => {
 	return (
